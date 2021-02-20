78a3f9df76e26f8662a18b82311dda062951e946
Dominick <dhing@stanford.edu>
2021-02-20 14:20:29 -0800

    Added utils.py file

--- utils.py
+++ utils.py
@@ -3,31 +3,71 @@ import os
 from scipy.io.wavfile import read as read_wav
 import pandas as pd
 import numpy as np
+import re
 
 RATE = 44100
 NCHANNELS = 2
 NFRAMES = 132299
 
+INST_PATTERN = '(\[)(...)(\])'
+
+INSTRUMENTS = {
+    'cel': 0,
+    'cla': 1,
+    'flu': 2,
+    'gac': 3,
+    'gel': 4,
+    'org': 5,
+    'pia': 6,
+    'sax': 7,
+    'tru': 8,
+    'vio': 9,
+    'voi': 10,
+}
+
+NINSTRUMENTS = 11
+
+
+def normalize_audio (arr):
+    norm = np.linalg.norm(arr)
+    return arr / norm
+
+
 # Walk through all files (recursively) under pathname
 # For each file that is a .wav, copy it's data into
-# the list
+# the list. Returns the full dataset, with labels,
+# as a Pandas DataFrame
 def load_wav_dataset (pathname):
     files_list = []
     dataPath = os.path.abspath(pathname)
     for root, dir, files in os.walk(dataPath, topdown = True):
         for file in files:
+            
+            # Examine only .wav files
             if file [-4:] == ".wav":
                 rate, data = read_wav (root + '/' + file)
-                dict = {
-                    "file": file,
-                    "dir": dir,
-                    "root": root,
-                    "rate": rate,
-                    "data": data,
-                    "nframes": data.shape[0],
-                    "nchannels": data.shape[1]
-                }
-                files_list.append(dict)
+                cats = np.zeros(shape=(NINSTRUMENTS, 1), dtype=np.int8)
+                
+                # Determines the type of instrument from the file name
+                match = re.search(INST_PATTERN, file)
+                if (match):
+                    cats[INSTRUMENTS[match.group(2)]] = 1
+                    
+                    dict = {
+                        "file": file,
+                        "dir": dir,
+                        "root": root,
+                        "rate": rate,
+                        "data": normalize_audio (data),
+                        "nframes": data.shape[0],
+                        "nchannels": data.shape[1],
+                        "label": cats,
+                        "instruments": match.group(2)
+                    }
+                    
+                    files_list.append(dict)
+    
+    # Returns the data as a Pandas DataFrame
     df = pd.DataFrame(files_list)
     return df
 
@@ -51,7 +91,7 @@ def combine_dataset (df, num_combinations, combine_size):
         if valid:
             dict = {
               "rate": RATE,
-              "data": sum,
+              "data": normalize_audio (sum),
               "nframes": sum.shape[0],
               "nchannels": sum.shape[1]
             }
