c830a83cfcb7fd83dbf1235c04645f82870c4072
Dominick Hing <dhing@stanford.edu>
2021-03-05 19:11:58 -0800

    Updated utils files (#6)

--- irmasTrainUtils.py
+++ irmasTrainUtils.py
@@ -7,6 +7,15 @@ from scipy.io.wavfile import write as write_wav
 import re
 
 
+def parse_label_train_label_from_string (label):
+    "Converts a string label into a numpy label. Strips '[' and ']' if they exist"
+    
+    new_label = label.lstrip('[').rstrip(']')
+    retval = np.zeros(shape=(utils.NINSTRUMENTS, 1), dtype=np.int8)
+    retval[utils.INSTRUMENTS[new_label],0] = 1
+    return retval
+
+
 def augment_song(rate, data):
     """
     Performs data augmentation. DATA is a numpy array with 2 channels with sample rate RATE.
@@ -75,4 +84,54 @@ def parse_irmas_trainset (source, dest):
                 count += 1
     
     return
-    
+
+
+
+
+def load_train_dataset (pathname):
+    """
+    Walk through the files (recursively) under pathname. For each file that is a .wav
+    copy its data into the list. Returns the full train set with labels as a Pandas
+    DataFrame
+    """
+    files_list = []
+    count = 0
+    dataPath = os.path.abspath(pathname)
+    for root, dir, files in os.walk(dataPath, topdown = True):
+        for file in files:
+            
+            if count % 500 == 0:
+                print("Count: ", count)
+            
+            # Examine only .wav files
+            if file[-4:] != ".wav":
+                continue
+            
+            rate, data = read_wav (root + '/' + file)
+            cats = np.zeros(shape=(utils.NINSTRUMENTS, 1), dtype=np.int8)
+
+            # Determines the type of instrument from the file name
+            match = re.search(utils.INST_PATTERN, file)
+            if (match):
+                cats[utils.INSTRUMENTS[match.group(2)]] = 1
+            #    cats = parse_label_train_label_from_string(match.group(2))
+            
+            dict = {
+                "file": file,
+                "dir": dir,
+                "root": root,
+                "rate": rate,
+                "data": utils.spectrogram_audio(utils.normalize_audio (data), rate),
+                "nframes": data.shape[0],
+                "nchannels": data.shape[1],
+                "label": cats,
+                "instruments": match.group(2)
+            }
+
+            files_list.append(dict)
+            count += 1
+
+    # Returns the data as a Pandas DataFrame
+    df = pd.DataFrame(files_list)
+    return df
+
